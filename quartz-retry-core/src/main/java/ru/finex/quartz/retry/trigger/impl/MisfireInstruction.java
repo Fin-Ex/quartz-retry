dd2c05ebcf97c46e8963511d0a529ad46d0091f5
BryanEstrada003 <HOME@DESKTOP-5NG0P5S>
2023-08-09 22:03:28 -0500

    Replace switch with conditionals.

--- quartz-retry-core/src/main/java/ru/finex/quartz/retry/trigger/impl/MisfireInstruction.java
+++ quartz-retry-core/src/main/java/ru/finex/quartz/retry/trigger/impl/MisfireInstruction.java
@@ -4,6 +4,7 @@
  */
 package ru.finex.quartz.retry.trigger.impl;
 
+import static org.quartz.Trigger.*;
 import static ru.finex.quartz.retry.trigger.RetryCronTrigger.*;
 
 /**
@@ -13,16 +14,17 @@ import static ru.finex.quartz.retry.trigger.RetryCronTrigger.*;
 public abstract class MisfireInstruction {
     
     public static MisfireInstruction createMisfireHandler(int misfireInstruction) {
-        switch (misfireInstruction) {
-            case MISFIRE_INSTRUCTION_DO_NOTHING:
-                return new MisfireInstDoNothing();
-            case MISFIRE_INSTRUCTION_FIRE_ONCE_NOW:
-                return new MisfireInsFireOnceNow();
-            case MISFIRE_INSTRUCTION_IGNORE_MISFIRE_POLICY:
-                return new MisfireInsIngnoreMisfirePolicy();
-            default:
-                return new MisfireInsDefault(misfireInstruction);
+        
+        if (misfireInstruction==MISFIRE_INSTRUCTION_DO_NOTHING){
+            return new MisfireInstDoNothing();
+        }else if(misfireInstruction==MISFIRE_INSTRUCTION_FIRE_ONCE_NOW){
+            return new MisfireInsFireOnceNow();
+        }else if(misfireInstruction== MISFIRE_INSTRUCTION_IGNORE_MISFIRE_POLICY){
+            return new MisfireInsIngnoreMisfirePolicy();
+        }else{
+            return new MisfireInsDefault(misfireInstruction);        
         }
+        
     }
     public abstract void getMisfireBehavior(RetryCronScheduleBuilder cb);
 
